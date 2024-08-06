778bd7a1aff1693f760aa6343b2b5a6ee617aaca
FJPintoR <fjpintoccv@gmail.com>
2024-08-05 23:38:24 -0400

    Guardando mejoras para edidar usurio y eliminar de la data

--- backend/controllers/controllerUsuarios.js
+++ backend/controllers/controllerUsuarios.js
@@ -1,9 +1,9 @@
 function guardarUsuario(usuario){
     try {
-        //const {id,nombreCompleto,correo,contrasenia}=usuario;
+       
         const sheetUsuarios = obtenerSheet(env_().SH_REGISTRO_USUARIO);
         Insert(JSON.parse (usuario),sheetUsuarios);
-        //sheetUsuarios.appendRow([id,nombreCompleto,correo,contrasenia]);
+
         return {
             titulo: "Registro exitoso",
             descripcion: "Ya se encuentra el usurio registrado en la base de datos."
@@ -13,12 +13,39 @@ function guardarUsuario(usuario){
             titulo:"Ops ha ocurrido un error! "+error,
             descripcion: "Por favor contacte a soporte."
         }
+   }
 
-        }
    
 } 
 
-function listarUsuarios(id=undefined){
-    //return obtenerDatos(env_().SH_REGISTRO_USUARIO);
-    return JSON.stringify(_read(obtenerSheet(env_().SH_REGISTRO_USUARIO),id));
+function listarUsuarios(id = undefined){
+    return JSON.stringify(_read(obtenerSheet(env_().SH_REGISTRO_USUARIO), id));
+}
+
+function actualizarUsuario (id,datos){
+    
+    try {
+        const sheetUsuarios = obtenerSheet(env_().SH_REGISTRO_USUARIO);
+        Update(id,datos,sheetUsuarios);
+        return {
+            titulo: "Actualizado correctamente",
+            descripcion: "Ya se encuentra el usurio actulizado en la base de datos."
+        }
+    } catch (error) {
+        return{
+            titulo:"Ops ha ocurrido un error! "+error,
+            descripcion: "Por favor contacte a soporte."
+        }
+   }
+   
+}
+
+function eliminarUsuario(id){
+    const sheetUsuarios=obtenerSheet(env_().SH_REGISTRO_USUARIO);
+    const usuarioAEliminar = _read(sheetUsuarios,id);
+    sheetUsuarios.deleteRow(usuarioAEliminar.row);
+    return{
+        titulo:"**Confirmaciòn**",
+        descripcion: "Usuario Eliminado."
+    }
 }
